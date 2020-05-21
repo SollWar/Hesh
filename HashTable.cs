4f24d26b7a9f00de82fb6cf748d89a00fe016886
SollWar <63407355+SollWar@users.noreply.github.com>
2020-05-21 17:26:03 +0300

    New

---
+++ HashTable.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hesh
+{
+    class HashTable
+    {
+        List<int>[] table;
+        public HashTable(int n)
+        {
+            table = new List<int>[n];
+            for (int i = 0; i < n; i++)
+            {
+                table[i] = new List<int> { };
+            }
+        }
+
+        public void Add(int key)
+        {
+            int index = key % table.Length;
+            table[index].Add(key);
+        }
+
+        public void Print()
+        {
+            for (int i = 0; i < table.Length; i ++)
+            {
+                Console.Write(i);
+                Console.Write("  ");
+                for (int j = 0; j < table[i].Count; j++)
+                {
+                    Console.Write(table[i][j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public bool Contains(int key)
+        {
+            bool flag = false;
+            for (int i = 0; i < table.Length; i++)
+                for (int j = 0; j < table[i].Count; j++)
+                {
+                    if (table[i][j] == key)
+                    flag = true;
+                }
+            return flag;
+        }
+
+        public int Count()
+        {
+            int k = 0;
+            for (int i = 0; i < table.Length; i++)
+                for (int j = 0; j < table[i].Count; j++)
+                    k++;
+            return k;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hesh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var init = new int[] { 7, 54, 20, 1, 45, 32, 10, 44 };
+
+            var ht = new HashTable(5);
+
+
+            foreach (var i in init)
+                ht.Add(i);
+
+            ht.Print();
+            Console.WriteLine(ht.Count());
+            Console.WriteLine(ht.Contains(7));
+            Console.ReadKey();
+        }
+    }
+}
